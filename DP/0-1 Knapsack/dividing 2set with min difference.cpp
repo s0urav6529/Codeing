23a5866647144f478111106ed68bdb47432b8447
Sourav Majumder Utsha <96060029+s0urav6529@users.noreply.github.com>
2025-04-07 10:07:38 +0600

    Update dividing 2set with min difference.cpp

--- DP/0-1 Knapsack/dividing 2set with min difference.cpp
+++ DP/0-1 Knapsack/dividing 2set with min difference.cpp
@@ -4,19 +4,9 @@
 ///this is O(n*totalsum)
 
 #include<bits/stdc++.h>
-#define Fast ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
-#define Freed freopen ("0in.txt","r",stdin);
-#define Fout freopen ("0out.txt","w",stdout);
 #define ll long long int
-#define pb push_back
-#define mp make_pair
-#define pi acos(-1.0)
-#define Mod 1000000007
-#define limit 102
-using namespace std;
-
 ll d[limit],total,n;
-ll dp[100002][limit];
+ll dp[100002][102];
 
 ll (ll sum, ll pos){
 
