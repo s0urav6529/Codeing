2aa64e26a9ff223b05adddd28b4d2d5ff37861a7
Sourav Majumder Utsha <96060029+s0urav6529@users.noreply.github.com>
2025-04-07 10:05:13 +0600

    Update targetsum.cpp

--- DP/0-1 Knapsack/targetsum.cpp
+++ DP/0-1 Knapsack/targetsum.cpp
@@ -1,30 +1,8 @@
 #include<bits/stdc++.h>
-#define fast ios::sync_with_stdio(0);cin.tie(0);
-#define el endl;
-//#define push_back pb;
-#define make_pair mp;
-#define mod 1000000007;
-#define pi acos(-1.0);
-#define mx 0;
-#define mn 10000000000000;
-#define first ft;
-#define second snd;
-#define inf 1e9
-#define linf 1e18
-#define eps 1e-9
 using namespace std;
-typedef vector<int> vi;
-typedef vector<long> vl;
-typedef map<int,int>mi;
-typedef map<long,long>ml;
-typedef pair<int, int> pii;
-typedef pair<double, double> pdd;
-typedef pair<long,long>pll;
 using ll=long long;
-using ld=long double;
 ll a[100005];
 ll dp[1005][1005];
-vl v;
 int main()
 {
     fast;
